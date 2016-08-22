33ff027dc22089d1e7865a9c8d8a848eb90bb70b
GoGoSphere <gogosphere@gmail.com>
2016-08-22 18:41:50 -0500

    clean up

--- puppet.go
+++ puppet.go
@@ -1,9 +1,5 @@
 package main
 
-// TODO: the hostList and hostFactValue need get an io.Reader from httpclient.
-// TODO: add optionparser for --action [--filter]
-// TODO: maybe kickout reuseable funtions or make them reusable.
-
 import (
 	"bytes"
 	"encoding/json"
@@ -11,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 type Details struct {
@@ -26,27 +24,22 @@ type Certmap struct {
 
 type certmap Certmap
 
-var url string
-var uri string
 var results map[string][]string
 
 func main() {
 
-	qanda := make(map[string][]string)
 	finder := make(map[string][]string)
 	var results map[string][]string
-	//cmdbase := strings.Split(os.Args[0], "/")
-	//fmt.Println(cmdbase[len(cmdbase)-1])
+	cmdbase := strings.Split(os.Args[0], "/")
+	fmt.Println(cmdbase[len(cmdbase)-1])
 
-	url := "http://aspdo046.aus1.homeaway.live:44880/v2"
-	qanda["osfamily"] = []string{"true", "false"}
-	qanda["is_virtual"] = []string{"Windows", "RedHat", "Ubuntu", "Debian"}
+	base := "http://aspdo046.aus1.homeaway.live:44880/v2"
 
 	endpoint := []string{"osfamily", "is_virtual"}
 	for _, v := range endpoint {
-		uri := url + "/facts/" + v
-		fmt.Println("sending to next function: ", uri)
-		results = fetchPuppetInfo(uri, finder)
+		url := base + "/facts/" + v
+		fmt.Println("sending to next function: ", url)
+		results = fetchPuppetInfo(url, finder)
 
 	}
 	for k, v := range results {
@@ -85,7 +78,6 @@ func hostList(pContents []byte, finder map[string][]string) map[string][]string 
 	json.Unmarshal(pContents, &r)
 
 	for _, v := range r {
-		//data := []string{v.Name + ":" + v.Value}
 
 		if len(finder[v.Certmap.Hostname]) == 0 {
 			finder[v.Certmap.Hostname] = []string{v.Name, v.Value}
@@ -97,28 +89,4 @@ func hostList(pContents []byte, finder map[string][]string) map[string][]string 
 	}
 	return finder
 
-}
-
-func hostFactValue(pContents []byte) {
-
-	var r []Details
-	json.Unmarshal(pContents, &r)
-	for k, v := range r {
-		fmt.Println(k, v.Certmap.Hostname, v.Name, v.Value)
-	}
-	fmt.Println(len(r))
-}
-
-func (a *Certmap) UnmarshalJSON(b []byte) (err error) {
-	j, s := certmap{}, ""
-	if err = json.Unmarshal(b, &j); err == nil {
-		*a = Certmap(j)
-		return
-	}
-	if err = json.Unmarshal(b, &s); err == nil {
-		a.Hostname = s
-		return
-	}
-
-	return
 }
